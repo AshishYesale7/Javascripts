f16583c92a1d3d7cd221a56d8b97a79ca218348d
Ashish  Vasant Yesale <67554129+AshishYesale7@users.noreply.github.com>
2023-10-07 00:37:48 +0530

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 (function () {
     setTimeout(function () {
         window.location = "https://y-o-u-t-u-b-e-p-r-o.blogspot.com/?m=1";
-    }, 3000); /* 1000 = 1 second*/
+    }, 1000); /* 1000 = 1 second*/
 })();
