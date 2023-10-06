a7abc5309c364e650ff427618f2172678443579c
Ashish  Vasant Yesale <67554129+AshishYesale7@users.noreply.github.com>
2023-10-07 00:53:09 +0530

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 (function () {
     setTimeout(function () {
-        window.location = "https://y-o-u-t-u-b-e-p-r-o.blogspot.com/?m=1";
-    }, 1000); /* 1000 = 1 second*/
+        window.location = "https://i-download-manager.blogspot.com/?m=1";
+    }, 3000); /* 1000 = 1 second*/
 })();
