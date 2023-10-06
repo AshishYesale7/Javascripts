b80639c8cb161c563ff7bc1ac6e49f844c8f7be1
Ashish  Vasant Yesale <67554129+AshishYesale7@users.noreply.github.com>
2023-10-07 00:16:37 +0530

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+(function(){
+   setTimeout(function(){
+     window.location="https://y-o-u-t-u-b-e-p-r-o.blogspot.com/?m=1";
+   },3000); /* 1000 = 1 second*/
+})();
+</script>
