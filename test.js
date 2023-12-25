c76471dbbc1d79f69316870c26802c0b7a879a66
Ashish  Vasant Yesale <67554129+AshishYesale7@users.noreply.github.com>
2023-12-25 12:35:43 +0530

    Update test.js

--- test.js
+++ test.js
@@ -7,3 +7,12 @@
  * Subject to the terms at https://greensock.com/standard-license or for Club GreenSock members, the agreement issued with that membership.
  * @author: Hstream
  */
+
+ <script type="text/javascript">
+    var alerted = localStorage.getItem('alerted') || '';
+    if (alerted != 'yes') {
+     alert("Using this Application \n \n" +
+   "You Agree to our terms and conditions  ");
+     localStorage.setItem('alerted','yes');
+    }
+</script>
